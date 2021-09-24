40349ac2ae13de5e30ea432cb11a44a02792a8b1
Fernando <Fernando@DESKTOP-306QFMM>
2021-09-24 19:12:54 -0300

    Incluindo testes de falha para métodos Fizz e Buzz.

---
+++ FizzBuzz/FizzBuzz.Logic/FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz.Logic
+{
+    public static class FizzBuzz
+    {
+        public static bool IsFizz(int number)
+        {
+            return false;
+        }
+
+        public static bool IsBuzz(int number)
+        {
+            return false;
+        }
+
+        public static bool IsFizzBuzz(int number)
+        {
+            return IsFizz(number) && IsBuzz(number);
+        }
+
+        public static string Get(int number)
+        {
+            if (IsFizz(number))
+                return "Fizz";
+            if (IsBuzz(number))
+                return "Buzz";
+            if (IsFizzBuzz(number))
+                return "FizzBuzz";
+            else
+                return number.ToString();
+        }
+
+        public static IList<string> Get(IList<int> numbers)
+        {
+            var result = new List<string>();
+
+            foreach(var number in numbers)
+            {
+                result.Add(Get(number));
+            }
+
+            //yield return
+            return result;
+        }
+    }
+}
